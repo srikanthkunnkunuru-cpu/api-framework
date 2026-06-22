98a4886b8fa2afa8d4ad1e3427cf4510fd2ad7a9
Srikanth Kunnkunuru <srikanthkunnkunuru@Mac.fritz.box>
2026-06-22 12:53:48 +0100

    first commit - API automation framework

---
+++ src/test/java/com/automation/PostAPITest.java
@@ -0,0 +1,32 @@
+package com.automation;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class PostAPITest {
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+public void createPost_shouldReturn201(){
+        String requestBody = "{\"title\": \"My First Post\", \"body\": \"Learning API automation\", \"userId\": 1}";
+
+        given()
+                .header("Content-type","application/Json")
+                .body(requestBody)
+                .when()
+                .post("posts")
+                .then()
+                .statusCode(201)
+                .body("title", equalTo("My First Post"))
+                .body("id",notNullValue());
+
+    }
+}
---
+++ src/test/java/com/automation/UserApiTest.java
@@ -0,0 +1,46 @@
+package com.automation;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class UserApiTest {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void getUser_shouldReturn200() {
+        given()
+            .when()
+                .get("/users/1")
+            .then()
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("name", equalTo("Leanne Graham"));
+    }
+
+    @Test
+    public void getUser_shouldHaveEmail() {
+        given()
+            .when()
+                .get("/users/1")
+            .then()
+                .statusCode(200)
+                .body("email", notNullValue());
+    }
+
+    @Test
+    public void getInvalidUser_shouldReturn404() {
+        given()
+            .when()
+                .get("/users/9999")
+            .then()
+                .statusCode(404);
+    }
+}
